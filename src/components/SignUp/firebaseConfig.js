f39e58c231a37ed32cdf118d126e80fecdeb7588
S.m.Samrat <imsamrat24@gmail.com>
2021-04-06 19:38:57 +0600

    firebase config setup

---
+++ src/components/SignUp/firebaseConfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDEiTKnmovGIwxhvi6drC3etNgO7FdBqpw",
+    authDomain: "foodwala24.firebaseapp.com",
+    projectId: "foodwala24",
+    storageBucket: "foodwala24.appspot.com",
+    messagingSenderId: "753004973666",
+    appId: "1:753004973666:web:3643dc39f891c450c6cb3b"
+  };
+export default firebaseConfig;
