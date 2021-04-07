c202608bbf99c34e34bb1c13bbcf30b600612633
S.m.Samrat <imsamrat24@gmail.com>
2021-04-07 18:09:21 +0600

    Updated Version

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './Banner.css'
 
 const Banner = () => {
+
+    const [searchQuery, setSearchQuery] = useState(null);
+    const getQuery = event => setSearchQuery(event.target.value);
+
     return (
-        <div>
-            
-        </div>
+        <section className='banner d-flex align-items-center text-center'>
+            <div className='container'>
+                <h1>Best Food Waiting For Your Belly</h1>
+
+                <div className='search-box col-md-6 my-5 mx-auto'>
+                    <input
+                        type="text"
+                        id="query"
+                        onChange={getQuery}
+                        className='form-control'
+                        placeholder='Search Food Item'
+                    />
+                    <Link to={'/search=' + searchQuery}>
+                        <button
+                            onClick={() => window.scrollBy(0, 525)}
+                            className='btn btn-danger search-btn btn-rounded'
+                        >
+                            Search
+                    </button>
+                    </Link>
+                </div>
+            </div>
+        </section>
     );
 };
 
